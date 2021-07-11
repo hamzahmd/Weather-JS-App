ceeed2192fc863aa5bcc0264be1e3bebdcb98c71
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-07-11 20:14:56 +0500

    Added Error Handling Notifications

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
+document.getElementById("search").addEventListener("click",getCityName);
 
-function getCityName(){
-  const city = document.querySelector("input.input-location").value;
+function showAlert(message, className){
+  const div = document.createElement('div');
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(message));
+
+  const container = document.querySelector('.container');
+    // Insert alert
+    const form = document.querySelector('.weather');
+    container.insertBefore(div,form);
   
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ef6e1d884b95c99db1a71a727fc19a1
-`
-fetch(api)
+    // Timeout after 2 sec
+    setTimeout(function(){
+      document.querySelector('.alert').remove();
+    },2000);
+  }
+
+
+
+async function getCityName(e){
+  let city = document.querySelector("input.input-location").value;
+  if (city === ''){
+    showAlert("No Entity found",'blank')
+  }
+  else {
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ef6e1d884b95c99db1a71a727fc19a1`;
+    
+
+    await fetch(api)
     .then(response => {
       return response.json();
     })
     .then((data) => {
-       const temperature = ((data.main.temp)-273.15).toFixed(1);
+       const temperature = Math.floor((data.main.temp)-273.15);
+       
        const icon = data.weather[0].icon;
        const description = data.weather[0].main;
        const humidity = data.main.humidity;
         const country = data.sys.country;
-      console.log(data)
+
        document.querySelector(".city").innerHTML = city;
        document.querySelector(".temp").innerHTML = `${temperature}°C`;
        document.querySelector(".icon").src =  `https://openweathermap.org/img/wn/${icon}@2x.png`;
        document.querySelector(".humidity").innerHTML = humidity;
        document.querySelector(".description").innerHTML = description;
        document.querySelector(".country").innerHTML = country;
-    })     
- 
+       document.querySelector("input.input-location").value = '';
+    })
+     .catch(function(){
+       showAlert('No Record of City','error404')
+       document.querySelector("input.input-location").value = '';
+     })
+  }
+  
+    e.preventDefault();
     }
-getCityName();
